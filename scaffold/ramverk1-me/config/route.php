a40f5d621264b64f4aaa3f6fe8d570c8e06d45fe
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-04 15:28:36 +0200

    trying out concept with first scaffold dir

---
+++ scaffold/ramverk1-me/config/route.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Routes.
+ */
+require __DIR__ . "/route/internal.php";
+require __DIR__ . "/route/debug.php";
+require __DIR__ . "/route/flat-file-content.php";
+require __DIR__ . "/route/404.php";
