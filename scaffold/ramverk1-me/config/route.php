58efc81b786ac4e7b647521dbb17b436cc7c66a3
Anders Nygren <litemerafrukt@gmail.com>
2017-08-09 14:39:24 +0200

    Add comment on route 404

--- scaffold/ramverk1-me/config/route.php
+++ scaffold/ramverk1-me/config/route.php
@@ -5,4 +5,8 @@
 require __DIR__ . "/route/internal.php";
 require __DIR__ . "/route/debug.php";
 require __DIR__ . "/route/flat-file-content.php";
+
+/**
+ * Catch all route that should be the last one
+ */
 require __DIR__ . "/route/404.php";
