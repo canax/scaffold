3aa3c733b4591697c4155d597cc435275da36516
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-10 08:06:25 +0200

    add comment for 404 route to be last in sequence

--- scaffold/ramverk1-me/config/route.php
+++ scaffold/ramverk1-me/config/route.php
@@ -6,7 +6,5 @@ require __DIR__ . "/route/internal.php";
 require __DIR__ . "/route/debug.php";
 require __DIR__ . "/route/flat-file-content.php";
 
-/**
- * Catch all route that should be the last one
- */
+// Catch all route last
 require __DIR__ . "/route/404.php";
