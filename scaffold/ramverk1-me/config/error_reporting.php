2060d63343e14eebca92207c6a8acccd7e3435ee
Mikael Roos <mikael.t.h.roos@gmail.com>
2017-08-10 09:28:56 +0200

    Enhance message for uncaught exceptions in error_reporting.php.

--- scaffold/ramverk1-me/config/error_reporting.php
+++ scaffold/ramverk1-me/config/error_reporting.php
@@ -18,7 +18,13 @@ ini_set('display_errors', 1);     // Display all errors
  *
  */
 set_exception_handler(function ($e) {
-    echo "Anax: Uncaught exception: <p>"
+    echo "<p>Anax: Uncaught exception:</p><p>Line "
+        . $e->getLine()
+        . " in file "
+        . $e->getFile()
+        . "</p><p><code>"
+        . get_class($e)
+        . "</code></p><p>"
         . $e->getMessage()
         . "</p><p>Code: "
         . $e->getCode()
