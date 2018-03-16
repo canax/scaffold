5235339143086209ef2b6d87d3722c1f342c5380
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-03-16 17:14:20 +0100

    oophp-me validation

--- scaffold/oophp-me/.scaffold/default/htdocs/js/main.js
+++ scaffold/oophp-me/.scaffold/default/htdocs/js/main.js
@@ -1,4 +1,8 @@
 /**
  * To show off JS works and can be integrated.
  */
-console.info("main.js ready and loaded.");
+(function() {
+    "use strict";
+
+    console.info("main.js ready and loaded.");
+})();
