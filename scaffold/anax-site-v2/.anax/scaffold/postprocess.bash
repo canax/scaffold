7192249bb3d04285bf5bdde75e3dc53a9875e2a4
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-12-13 22:44:58 +0100

    Add anax-site-v2

---
+++ scaffold/anax-site-v2/.anax/scaffold/postprocess.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Postprocess scaffold
+#
+
+# Include ./functions.bash
+source .anax/scaffold/functions.bash
+
+# Install using composer
+composer install --no-dev
+
+# Get scaffolding scripts from Anax Lite and Anax Flat
+rsync -a vendor/anax/anax/.anax/scaffold/postprocess.d .anax/scaffold/
+
+# Run scaffolding scripts
+echo -n "Processing scaffolding scripts: "
+for file in .anax/scaffold/postprocess.d/*.bash; do
+    echo -n "."
+    bash "$file"
+done
+echo " done."
