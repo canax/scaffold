0464c22596c6fc12637836a6ab6a3c1d7fb0953c
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-10-23 14:40:49 +0200

    Update to build anax controller

--- scaffold/anax-controller/CLASS_NAME.php
+++ scaffold/anax-controller/CLASS_NAME.php
@@ -4,47 +4,126 @@ namespace NAMESPACE;
 
 use \Anax\Configure\ConfigureInterface;
 use \Anax\Configure\ConfigureTrait;
-use \Anax\DI\InjectionAwareInterface;
-use \Anax\Di\InjectionAwareTrait;
+use \Anax\Commons\ContainerInjectableInterface;
+use \Anax\Commons\ContainerInjectableTrait;
 
 /**
  * A controller class.
  */
-class CLASS_NAME implements ConfigureInterface, InjectionAwareInterface
+class CLASS_NAME
+    implements ConfigureInterface,
+               ContainerInjectableInterface
 {
     use ConfigureTrait,
-        InjectionAwareTrait;
+        ContainerInjectableTrait;
 
 
 
     /**
-     * @var $data description
+     * @var string $db a sample member variable that gets initialised
      */
-    private $data;
+    private $db = "not active";
 
 
 
     /**
-     * Description.
-     *
-     * @param $data description.
-     *
-     * @throws Exception
+     * The initialize method is optional and will always be called before the
+     * target method/action. This is a convienient method where you could
+     * setup internal properties that are commonly used by several methods.
      *
      * @return void
      */
-    public function getIndex($data)
+    public function initialize() : void
     {
-        $title      = "A index page";
-        $view       = $this->di->get("view");
-        $pageRender = $this->di->get("pageRender");
+        // Use to initialise member variables.
+        $this->db = "active";
+    }
+
+
+
+    /**
+     * This is how a general helper method can be created in the controller.
+     *
+     * @param string $method as the method that handled the controller
+     *                       action.
+     * @param array  $args   as an array of arguments.
+     *
+     * @return string as a message to output to help understand how the
+     *                controller method works.
+     */
+    public function getDetailsOnRequest(
+        string $method,
+        array $args = []
+    ) : string
+    {
+        $request     = $this->di->get("request");
+        $path        = $request->getRoute();
+        $httpMethod  = $request->getMethod();
+        $numArgs     = count($args);
+        $strArgs     = implode(", ", $args);
+        $queryString = http_build_query($request->getGet(), '', ', ');
+
+        return <<<EOD
+            <h1>$method</h1>
+
+            <p>The request was '$path' ($httpMethod).
+            <p>Got '$numArgs' arguments: '$strArgs'.
+            <p>Query string contains: '$queryString'.
+            <p>\$db is '{$this->db}'.
+        EOD;
+    }
+
+
+
+    /**
+     * This is the index method action, it handles:
+     *  ANY METHOD mountpoint
+     *  ANY METHOD mountpoint/
+     *  ANY METHOD mountpoint/index
+     *
+     * @return object response to render.
+     */
+    public function indexAction()
+    {
+        $page    = $this->di->get("page");
 
         $data = [
-            "content" => "A index page",
+            "content" => $this->getDetailsOnRequest(__METHOD__),
         ];
+        $page->add("anax/v2/article/default", $data);
 
-        $view->add("default2/article", $data);
+        return $page->render([
+            "title" => __METHOD__,
+        ]);
+    }
 
-        $pageRender->renderPage(["title" => $title]);
+
+
+    /**
+     * Adding an optional catchAll() method will catch all actions sent to the
+     * router. You can then reply with an actual response or return void to
+     * allow for the router to move on to next handler.
+     * A catchAll() handles the following, if a specific action method is not
+     * created:
+     *  ANY METHOD mountpoint/**
+     *
+     * @param array $args as a variadic parameter.
+     *
+     * @return object response to render.
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function catchAll(...$args)
+    {
+        $page    = $this->di->get("page");
+
+        $data = [
+            "content" => $this->getDetailsOnRequest(__METHOD__, $args),
+        ];
+        $page->add("anax/v2/article/default", $data);
+
+        return $page->render([
+            "title" => __METHOD__,
+        ]);
     }
 }
